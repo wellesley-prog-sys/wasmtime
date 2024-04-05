ab7ef82bf86b09981a0f905702a5beb8bdc350be
AJ Chau <chaujannette@gmail.com>
2024-04-05 01:37:08 -0400

    Extract Rule from Defs

--- cranelift/isle/veri/isle_fuzzing/src/expr.rs
+++ cranelift/isle/veri/isle_fuzzing/src/expr.rs
@@ -1,5 +1,5 @@
 use cranelift_isle as isle;
-use isle::ast::{Decl, Defs};
+use isle::ast::*;
 use isle::sema::{Pattern, TermEnv, TypeEnv, VarId};
 use std::path::Path;
 use isle::lexer::Pos;
@@ -22,16 +22,32 @@ pub struct Inst {
 }
 
 
-fn convert_rules(filename: impl AsRef<Path>) -> Vec<Expr>{
+fn convert_rules(filename: impl AsRef<Path>) -> Vec<cranelift_isle::ast::Rule>{
     let lexer = isle::lexer::Lexer::from_files([&filename]).unwrap();
     let defs = isle::parser::parse(lexer).expect("should parse");
-    dbg!(&defs);
-    todo!()
-    //return defs;
-    //Defs = parse(filename) => defs.iter().filter() matches (Rule!) (just keep the definitions that are rules
+
+    let rules: Vec<_> = defs.defs.iter().filter_map(|def| {
+        if let Def::Rule(rule) = def {
+            Some(rule)
+        } else {
+            None
+        }
+    }).cloned().collect(); 
+
+    //dbg!(&defs);
+    //println!(defs);
+    return rules;
 }
 
+
 fn main() {
-    let toPrint: Vec<Expr> = convert_rules("construct_and_extract.isle");
-    println!("{:?}",toPrint);
+    let to_print: Vec<cranelift_isle::ast::Rule> = convert_rules("construct_and_extract.isle");
+    println!("{:?}",to_print);
+
+    // for rule in to_print {
+    //     let pattern = rule.pattern;
+    //     // Now you can use `pattern` as needed
+    //     println!("{:?}", pattern);
+    // }
+    // dbg!(&pattern)
 }
