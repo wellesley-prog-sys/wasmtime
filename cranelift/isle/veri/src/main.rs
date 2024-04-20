ae0f51f7cc50d52c20cb0ed9ced3a2d2dd1eb287
Michael McLoughlin <mcloughlin@cmu.edu>
2024-04-20 13:45:26 -0400

    skeleton ISLE reader
    Original-commit: https://github.com/mmcloughlin/veriisle-wasmtime/commit/c93b1f50648e10b31950b8585d1548935e629d70

--- cranelift/isle/veri/src/main.rs
+++ cranelift/isle/veri/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cranelift_codegen_meta::{generate_isle, isle::get_isle_compilations};
+
+#[derive(Parser)]
+struct Opts {
+    /// Name of the ISLE compilation.
+    #[arg(long, required = true)]
+    name: String,
+
+    /// Path to codegen crate directory.
+    #[arg(long, required = true)]
+    codegen_crate_dir: std::path::PathBuf,
+
+    /// Working directory.
+    #[arg(long, required = true)]
+    work_dir: std::path::PathBuf,
+}
+
+impl Opts {
+    fn isle_input_files(&self) -> anyhow::Result<Vec<std::path::PathBuf>> {
+        // Generate ISLE files.
+        let gen_dir = &self.work_dir;
+        generate_isle(gen_dir)?;
+
+        // Lookup ISLE compilations.
+        let compilations = get_isle_compilations(&self.codegen_crate_dir, gen_dir);
+
+        // Return inputs from the matching compilation, if any.
+        Ok(compilations
+            .lookup(&self.name)
+            .ok_or(anyhow::format_err!(
+                "unknown ISLE compilation: {}",
+                self.name
+            ))?
+            .inputs())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let inputs = opts.isle_input_files()?;
+    println!("inputs = {inputs:?}");
+    Ok(())
 }
