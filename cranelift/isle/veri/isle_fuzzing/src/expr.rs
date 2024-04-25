1b1f97eaa6578fd758591bb1eda6e19b4d24409c
AJ Chau <chaujannette@gmail.com>
2024-04-24 22:00:57 -0400

    Identify Instructions

--- cranelift/isle/veri/isle_fuzzing/src/expr.rs
+++ cranelift/isle/veri/isle_fuzzing/src/expr.rs
@@ -6,6 +6,8 @@ use std::path::Path;
 use crate::isle::ast;
 use isle::lexer::Pos;
 use isle::compile::create_envs;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Expr {
@@ -23,28 +25,58 @@ pub struct Inst {
     pub args: Vec<Expr>,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct NotAnInst {
+    pub name: Ident,
+    pub args: Vec<Expr>,
+}
+
 
 fn convert_pattern(pattern: &ast::Pattern, typeenv: &TypeEnv) -> Expr {
     match pattern {
-        ast::Pattern::Var {var,pos} => {
+        ast::Pattern::Var { var, .. } => {
             let name = var.0.clone();
             Expr::Var(name)
         }, 
         ast::Pattern::ConstInt { val, .. } => Expr::Int(*val),
         ast::Pattern::Term { sym, args, .. } => {
-            let converted_args = args.iter().map(|a| convert_pattern(a,typeenv)).collect();
-            Expr::Inst(
-                Inst {
-                    name: Ident(sym.0.clone(), Pos::default()), //Pos????
-                    //check against list of instructions vs. name, if not inst, label notinst
+            let converted_args = args.iter().map(|a| convert_pattern(a, typeenv)).collect();
+            let instr = lines_from_file("instructions.txt");
+            let name = sym.0.clone();
+            let is_inst_in_list = instr.contains(&name);
+            if is_inst_in_list {
+                Expr::Inst(
+                    Inst {
+                        name: Ident(sym.0.clone(), Pos::default()),
+                        args: converted_args,
+                    }
+                )
+            } else {
+                let not_an_inst_expr = NotAnInst {
+                    name: Ident(sym.0.clone(), Pos::default()),
                     args: converted_args,
-            })
+                };
+                Expr::NotAnInst(not_an_inst_expr.args)
+            }
         }
         ast::Pattern::Wildcard { .. } => todo!(), 
-        ast::Pattern::BindPattern{..} => todo!(),
+        ast::Pattern::BindPattern {..} => todo!(),
         _ => todo!(), 
     }
 }
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+// fn is_instruction(exprs: Vec<Expr>) -> bool {
+//     let instr = lines_from_file("instructions.txt");
+
+// }
 
 
 fn convert_rules(filename: impl AsRef<Path>) -> Vec<Expr>{
@@ -78,11 +110,13 @@ fn convert_rules(filename: impl AsRef<Path>) -> Vec<Expr>{
 }
 
 
+
 fn main() {
 
-    // let to_print: Vec<Expr> = convert_rules("amod_unextended.isle");
-    let to_print: Vec<Expr> = convert_rules("construct_and_extract.isle");
+    //let to_print: Vec<Expr> = convert_rules("amod_unextended.isle");
+    let to_print: Vec<Expr> = convert_rules("amod_unextended.isle");
     println!("{:?}",to_print);
+    }
 
     // for rule in to_print {
         // let pattern = &rule.pattern;
@@ -91,7 +125,6 @@ fn main() {
         // println!("{:?}", rule);
     // }
     // dbg!(&pattern)
-}
 
 // TODO 4/16
 // 1. check term against list of instructions --> only want isntructions, rest be notinst.
@@ -101,3 +134,19 @@ fn main() {
 
 
     // cargo run --bin expr
+
+
+
+/*pub fn find_instr(exprs: Vec<Expr>) -> Vec<String> {
+    let instr = lines_from_file("instructions.txt");
+    let mut filtered_instr = Vec::new();
+    for expr in exprs {
+        if let Expr::Inst(inst) = expr {
+            if let Ident(ident, _) = inst.name {
+                filtered_instr.push(ident);
+            }
+        }
+    }
+    filtered_instr
+}
+*/
