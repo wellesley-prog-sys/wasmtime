2c58d453d103925ec1ebfde7d38c9c55a78beb77
AJ Chau <chaujannette@gmail.com>
2024-05-08 17:59:55 -0400

    aUTOMATION!

--- cranelift/isle/veri/isle_fuzzing/automate_script.sh
+++ cranelift/isle/veri/isle_fuzzing/automate_script.sh
@@ -1,27 +1,51 @@
 EXPR_RS_PATH="/Users/ajchau/Documents/GitHub/wasmtime/cranelift/isle/veri/isle_fuzzing/src/expr.rs"
-
-TEST_CLIF_PATH="/Users/ajchau/Documents/GitHub/wasmtime/cranelift/testauto.clif"
-
+EXPR_DIR="/Users/ajchau/Documents/GitHub/wasmtime/cranelift/isle/veri/isle_fuzzing/src/"
+TEST_CLIF_PATH="/Users/ajchau/Documents/GitHub/wasmtime/cranelift/isle/veri/isle_fuzzing/testauto.clif"
 DIR_PATH="/Users/ajchau/Documents/GitHub/wasmtime/cranelift/"
 
-# compile and run expr.rs, and get the output
-TEST_CLIF=$(cargo run --bin expr)
+while true; do
 
-# write the output program to testauto.clif file in the cranelift dir
-echo "$TEST_CLIF" > "$TEST_CLIF_PATH"
+    # Run expr.rs and save the output to testauto.clif under isle_fuzzing
+    TEST_CLIF=$(cargo run --bin expr)
+    echo "$TEST_CLIF" > "$TEST_CLIF_PATH"
+
+    cd "$DIR_PATH"
+
+    # Run testauto.clif through cranelift (only wors on mac)
+    OUTPUT=$(cargo run -- compile --target aarch64 -D "$TEST_CLIF_PATH")
+    echo "Output:"
+    echo "$OUTPUT"
+
+    # Check if the output starts with ".byte", which means the generated clif is executable
+    if [[ "$OUTPUT" == .byte* ]]; then
+        echo "Condition met: The output starts with '.byte'"
+        break
+    fi
+
+    # Wait for a while before trying again
+    sleep 1
+
+    # go back to the dir with expr.rs to build and run again
+    cd "$EXPR_DIR"
 
 
-# cd back to cranelift dir
-cd "$DIR_PATH"
+done
 
-cargo run -- compile --target aarch64 -D testauto.clif
-
+echo "complete. Found executable random gen .clif and output ARM"
 
 
 # to run this script
+<<<<<<< Updated upstream
 # first substitute the paths with your local paths. 
 # EXPR_RS_PATH is where expr.rs is located. 
 # TEST_CLIF_PATH should be the path to the generated code stored in testauto.clif file in cranelift folder
 # DIR_PATH should be the path to your local cranelift folder                                              
+=======
+# first update the paths with your local paths. 
+    # EXPR_RS_PATH is where expr.rs is located. 
+    # EXPR_DIR is the folder where expr.rs is located
+    # TEST_CLIF_PATH should be the path to the generated code stored in testauto.clif file in cranelift folder
+    # DIR_PATH should be the path to your local cranelift folder                                              
+>>>>>>> Stashed changes
 # then chmod +x automate_script.sh to make sure it is executable
 # run the script: ./automate_script.sh
