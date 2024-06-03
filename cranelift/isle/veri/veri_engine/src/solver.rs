03da9bb31ba1199f68fc2a51dfff4779586c7f58
vaishuc17 <153235509+vaishuc17@users.noreply.github.com>
2024-06-03 14:30:55 -0400

    Added if-let pretty-printing (#3)
    * Added if-let petty-printing
    * Fixed snake case issue within display_model

--- cranelift/isle/veri/veri_engine/src/solver.rs
+++ cranelift/isle/veri/veri_engine/src/solver.rs
@@ -1564,6 +1564,26 @@ impl SolverCtx {
         );
         println!("{}", self.smt.display(lhs));
 
+        // if-let statement processing
+        print!("(if-let "); 
+        for if_let_struct in &rule.iflets {
+            let if_lhs = &if_let_struct.lhs;
+            let if_rhs: &cranelift_isle::sema::Expr  = &if_let_struct.rhs;
+
+            let if_lhs_expr = self.display_isle_pattern(
+                termenv,
+                typeenv,
+                &vars,
+                rule,
+                &if_lhs, 
+            );
+    
+            let if_rhs_expr = self.display_isle_expr(termenv, typeenv, &vars, rule, &if_rhs); 
+
+            print!("({} {})\n", self.smt.display(if_lhs_expr), self.smt.display(if_rhs_expr)); 
+        }
+        print!(")\n");
+
         println!("=>");
         let rhs = self.display_isle_expr(termenv, typeenv, &vars, rule, &rule.rhs);
         println!("{}", self.smt.display(rhs));
