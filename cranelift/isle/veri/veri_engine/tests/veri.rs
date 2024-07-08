499a493e1ab1b34419bddf36a5d3c8cc6e98a1eb
Vaishu Chintam <jc103@wellesley.edu>
2024-07-08 15:57:15 -0400

    WIP unit tests

--- cranelift/isle/veri/veri_engine/tests/veri.rs
+++ cranelift/isle/veri/veri_engine/tests/veri.rs
@@ -3497,3 +3497,19 @@ fn test_named_load_sub64_x64_movzx() {
         )
     });
 }
+
+#[test]
+fn test_load_conditional() {
+    run_and_retry(|| {
+        test_from_file_with_lhs_termname_simple(
+            "./examples/load/load_conditional.isle",
+            "lhs".to_string(),
+            vec![
+                (Bitwidth::I8, VerificationResult::Success),
+                (Bitwidth::I16, VerificationResult::Success),
+                (Bitwidth::I32, VerificationResult::Success),
+                (Bitwidth::I64, VerificationResult::Success),
+            ],
+        )
+    });
+}
