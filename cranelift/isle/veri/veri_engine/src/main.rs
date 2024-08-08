d34dbf409006dab12ac450ba9ce5c1658b99df19
Alexa VanHattum <amv94@cornell.edu>
2024-08-08 15:37:39 -0400

    Squash 08-08

---
+++ cranelift/isle/veri/veri_engine/src/main.rs
@@ -0,0 +1,132 @@
+//! Prototype verification tool for Cranelift's ISLE lowering rules.
+
+use clap::{ArgAction, Parser};
+use std::env;
+use std::path::PathBuf;
+use veri_engine_lib::verify::verify_rules;
+use veri_engine_lib::{build_clif_lower_isle, Config};
+
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Args {
+    /// Sets the input file
+    #[clap(short, long)]
+    input: Option<String>,
+
+    /// Which LHS root to verify
+    #[clap(short, long, default_value = "lower")]
+    term: String,
+
+    /// Which width types to verify
+    #[clap(long)]
+    widths: Option<Vec<String>>,
+
+    /// Which named rule to verify
+    #[clap(long)]
+    names: Option<Vec<String>>,
+
+    /// Don't use the prelude ISLE files
+    #[clap(short, long, action=ArgAction::SetTrue)]
+    noprelude: bool,
+
+    /// Include the aarch64 files
+    #[clap(short, long, action=ArgAction::SetTrue)]
+    aarch64: bool,
+
+    /// Include the x64 files
+    #[clap(short, long, action=ArgAction::SetTrue)]
+    x64: bool,
+
+    /// Don't check for distinct possible models
+    #[clap(long, action=ArgAction::SetTrue)]
+    nodistinct: bool,
+}
+
+fn main() {
+    env_logger::init();
+
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+
+    let args = Args::parse();
+    let mut inputs = vec![];
+
+    let valid_widths = ["I8", "I16", "I32", "I64"];
+
+    if let Some(widths) = &args.widths {
+        for w in widths {
+            let w_str = w.as_str();
+            if !valid_widths.contains(&w_str) {
+                panic!("Invalid width type: {}", w);
+            }
+        }
+    }
+
+    if !args.noprelude {
+        // Build the relevant ISLE prelude using the meta crate
+        inputs.push(build_clif_lower_isle());
+
+        // TODO: clean up path logic
+        inputs.push(cur_dir.join("../../../codegen/src").join("inst_specs.isle"));
+        inputs.push(cur_dir.join("../../../codegen/src").join("prelude.isle"));
+        inputs.push(
+            cur_dir
+                .join("../../../codegen/src")
+                .join("prelude_lower.isle"),
+        );
+    }
+
+    if args.aarch64 {
+        inputs.push(
+            cur_dir
+                .join("../../../codegen/src/isa/aarch64")
+                .join("inst.isle"),
+        );
+        inputs.push(
+            cur_dir
+                .join("../../../codegen/src/isa/aarch64")
+                .join("lower.isle"),
+        );
+        if args.input.is_some() {
+            panic!("Cannot specify both input file and aarch64 mode.")
+        }
+    } else if args.x64 {
+        inputs.push(
+            cur_dir
+                .join("../../../codegen/src/isa/x64")
+                .join("inst.isle"),
+        );
+        inputs.push(
+            cur_dir
+                .join("../../../codegen/src/isa/x64")
+                .join("lower.isle"),
+        );
+        if args.input.is_some() {
+            panic!("Cannot specify both input file and x64 mode.")
+        }
+    } else {
+        if let Some(i) = args.input {
+            inputs.push(PathBuf::from(i));
+        } else {
+            panic!("Missing input file in non-aarch64 mode");
+        }
+    }
+
+    let names = if let Some(names) = args.names {
+        let mut names = names;
+        names.sort();
+        names.dedup();
+        Some(names)
+    } else {
+        None
+    };
+
+    let config = Config {
+        term: args.term,
+        names: names,
+        distinct_check: !args.nodistinct,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+    };
+
+    verify_rules(inputs, &config, &args.widths)
+}
---
+++ cranelift/isle/veri/veri_engine/tests/utils/mod.rs
@@ -0,0 +1,436 @@
+use cranelift_isle::compile::create_envs;
+use std::env;
+use std::path::PathBuf;
+use std::time::Duration;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+use veri_engine_lib::annotations::parse_annotations;
+use veri_engine_lib::build_clif_lower_isle;
+use veri_engine_lib::type_inference::type_rules_with_term_and_types;
+use veri_engine_lib::verify::verify_rules_for_term;
+use veri_engine_lib::Config;
+use veri_ir::{ConcreteTest, Counterexample, TermSignature, VerificationResult};
+
+#[derive(Debug, EnumIter, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[repr(usize)]
+pub enum Bitwidth {
+    I8 = 8,
+    I16 = 16,
+    I32 = 32,
+    I64 = 64,
+}
+
+pub enum TestResult {
+    Simple(Vec<(Bitwidth, VerificationResult)>),
+    Expect(fn(&TermSignature) -> VerificationResult),
+}
+
+type TestResultBuilder = dyn Fn(Bitwidth) -> (Bitwidth, VerificationResult);
+
+// Some examples of functions we might need
+#[allow(dead_code)]
+pub fn just_8_result() -> TestResult {
+    TestResult::Simple(vec![(Bitwidth::I8, VerificationResult::Success)])
+}
+
+#[allow(dead_code)]
+pub fn just_16_result() -> TestResult {
+    TestResult::Simple(vec![(Bitwidth::I16, VerificationResult::Success)])
+}
+
+#[allow(dead_code)]
+pub fn just_32_result() -> TestResult {
+    TestResult::Simple(vec![(Bitwidth::I32, VerificationResult::Success)])
+}
+
+#[allow(dead_code)]
+pub fn just_64_result() -> TestResult {
+    TestResult::Simple(vec![(Bitwidth::I64, VerificationResult::Success)])
+}
+
+/// All bitwidths verify
+#[allow(dead_code)]
+pub fn all_success_result() -> Vec<(Bitwidth, VerificationResult)> {
+    custom_result(&|w| (w, VerificationResult::Success))
+}
+
+/// All bitwidths fail
+#[allow(dead_code)]
+pub fn all_failure_result() -> Vec<(Bitwidth, VerificationResult)> {
+    custom_result(&|w| (w, VerificationResult::Failure(Counterexample {})))
+}
+
+/// Specify a custom set expected result (helpful if you want to test all the bitwidths and expect
+/// a range of different success, failure, and inapplicable outcomes)
+pub fn custom_result(f: &TestResultBuilder) -> Vec<(Bitwidth, VerificationResult)> {
+    Bitwidth::iter().map(|w| f(w)).collect()
+}
+
+fn test_rules_with_term(inputs: Vec<PathBuf>, tr: TestResult, config: Config) -> () {
+    let lexer = cranelift_isle::lexer::Lexer::from_files(&inputs).unwrap();
+    let defs = cranelift_isle::parser::parse(lexer).expect("should parse");
+    let (typeenv, termenv) = create_envs(&defs).unwrap();
+    let annotation_env = parse_annotations(&defs, &termenv, &typeenv);
+
+    let term_signatures = annotation_env
+        .get_term_signatures_by_name(&termenv, &typeenv)
+        .get(config.term.as_str())
+        .expect(format!("Missing term width for {}", config.term).as_str())
+        .clone();
+    let instantiations = match tr {
+        TestResult::Simple(s) => {
+            let mut res = vec![];
+            for (width, result) in s {
+                let ty = match width {
+                    Bitwidth::I8 => veri_ir::Type::BitVector(Some(8)),
+                    Bitwidth::I16 => veri_ir::Type::BitVector(Some(16)),
+                    Bitwidth::I32 => veri_ir::Type::BitVector(Some(32)),
+                    Bitwidth::I64 => veri_ir::Type::BitVector(Some(64)),
+                };
+                // Find the type instantiations with this as the canonical type
+                let all_instantiations: Vec<&TermSignature> = term_signatures
+                    .iter()
+                    .filter(|sig| sig.canonical_type.unwrap() == ty)
+                    .collect();
+                if all_instantiations.len() < 1 {
+                    panic!("Missing type instantiation for width {:?}", width);
+                }
+                for i in all_instantiations {
+                    res.push((i.clone(), result.clone()));
+                }
+            }
+            res
+        }
+        TestResult::Expect(expect) => term_signatures
+            .iter()
+            .map(|sig| (sig.clone(), expect(sig)))
+            .collect(),
+    };
+
+    for (type_instantiation, expected_result) in instantiations {
+        println!("Expected result: {:?}", expected_result);
+        let type_sols = type_rules_with_term_and_types(
+            &termenv,
+            &typeenv,
+            &annotation_env,
+            &config,
+            &type_instantiation,
+            &None,
+        );
+        let result = verify_rules_for_term(
+            &termenv,
+            &typeenv,
+            &type_sols,
+            type_instantiation,
+            &None,
+            &config,
+        );
+        assert_eq!(result, expected_result);
+    }
+}
+
+pub fn test_from_file_with_lhs_termname_simple(
+    file: &str,
+    termname: String,
+    tr: Vec<(Bitwidth, VerificationResult)>,
+) -> () {
+    test_from_file_with_lhs_termname(file, termname, TestResult::Simple(tr))
+}
+
+pub fn test_from_file_with_lhs_termname(file: &str, termname: String, tr: TestResult) -> () {
+    println!("Verifying {} rules in file: {}", termname, file);
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![prelude_isle, prelude_lower_isle, clif_isle];
+    inputs.push(build_clif_lower_isle());
+    inputs.push(PathBuf::from(file));
+    let config = Config {
+        term: termname,
+        distinct_check: true,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+        names: None,
+    };
+    test_rules_with_term(inputs, tr, config);
+}
+
+pub fn test_aarch64_rule_with_lhs_termname_simple(
+    rulename: &str,
+    termname: &str,
+    tr: Vec<(Bitwidth, VerificationResult)>,
+) -> () {
+    test_aarch64_rule_with_lhs_termname(rulename, termname, TestResult::Simple(tr))
+}
+
+pub fn test_aarch64_rule_with_lhs_termname(rulename: &str, termname: &str, tr: TestResult) -> () {
+    println!("Verifying rule `{}` with termname {} ", rulename, termname);
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("inst.isle"),
+    );
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("lower.isle"),
+    );
+    let config = Config {
+        term: termname.to_string(),
+        distinct_check: true,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+        names: Some(vec![rulename.to_string()]),
+    };
+    test_rules_with_term(inputs, tr, config);
+}
+
+pub fn test_x64_rule_with_lhs_termname_simple(
+    rulename: &str,
+    termname: &str,
+    tr: Vec<(Bitwidth, VerificationResult)>,
+) -> () {
+    test_x64_rule_with_lhs_termname(rulename, termname, TestResult::Simple(tr))
+}
+
+pub fn test_x64_rule_with_lhs_termname(rulename: &str, termname: &str, tr: TestResult) -> () {
+    println!("Verifying rule `{}` with termname {} ", rulename, termname);
+    // TODO(mbm): dedupe with aarch64
+    // TODO(mbm): share configuration with cranelift/codegen/build.rs (or export it as part of build)
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/x64")
+            .join("inst.isle"),
+    );
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/x64")
+            .join("lower.isle"),
+    );
+    let config = Config {
+        term: termname.to_string(),
+        distinct_check: true,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+        names: Some(vec![rulename.to_string()]),
+    };
+    test_rules_with_term(inputs, tr, config);
+}
+
+pub fn test_from_file_with_config_simple(
+    file: &str,
+    config: Config,
+    tr: Vec<(Bitwidth, VerificationResult)>,
+) -> () {
+    test_from_file_with_config(file, config, TestResult::Simple(tr))
+}
+pub fn test_from_file_with_config(file: &str, config: Config, tr: TestResult) -> () {
+    println!("Verifying {} rules in file: {}", config.term, file);
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(PathBuf::from(file));
+    test_rules_with_term(inputs, tr, config);
+}
+
+pub fn test_aarch64_with_config_simple(
+    config: Config,
+    tr: Vec<(Bitwidth, VerificationResult)>,
+) -> () {
+    test_aarch64_with_config(config, TestResult::Simple(tr))
+}
+
+pub fn test_aarch64_with_config(config: Config, tr: TestResult) -> () {
+    println!(
+        "Verifying rules {:?} with termname {}",
+        config.names, config.term
+    );
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("inst.isle"),
+    );
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("lower.isle"),
+    );
+    test_rules_with_term(inputs, tr, config);
+}
+
+pub fn test_concrete_aarch64_rule_with_lhs_termname(
+    rulename: &str,
+    termname: &str,
+    concrete: ConcreteTest,
+) -> () {
+    println!(
+        "Verifying concrete input rule `{}` with termname {} ",
+        rulename, termname
+    );
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("inst.isle"),
+    );
+    inputs.push(
+        cur_dir
+            .join("../../../codegen/src/isa/aarch64")
+            .join("lower.isle"),
+    );
+
+    let lexer = cranelift_isle::lexer::Lexer::from_files(&inputs).unwrap();
+    let defs = cranelift_isle::parser::parse(lexer).expect("should parse");
+    let (typeenv, termenv) = create_envs(&defs).unwrap();
+    let annotation_env = parse_annotations(&defs, &termenv, &typeenv);
+
+    let config = Config {
+        term: termname.to_string(),
+        distinct_check: false,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+        names: Some(vec![rulename.to_string()]),
+    };
+
+    // Get the types/widths for this particular term
+    let args = concrete.args.iter().map(|i| i.ty.clone()).collect();
+    let ret = concrete.output.ty;
+    let t = TermSignature {
+        args,
+        ret,
+        canonical_type: None,
+    };
+
+    let type_sols = type_rules_with_term_and_types(
+        &termenv,
+        &typeenv,
+        &annotation_env,
+        &config,
+        &t,
+        &Some(concrete.clone()),
+    );
+    let result = verify_rules_for_term(&termenv, &typeenv, &type_sols, t, &Some(concrete), &config);
+    assert_eq!(result, VerificationResult::Success);
+}
+
+pub fn test_concrete_input_from_file_with_lhs_termname(
+    file: &str,
+    termname: String,
+    concrete: ConcreteTest,
+) -> () {
+    println!(
+        "Verifying concrete input {} rule in file: {}",
+        termname, file
+    );
+    // TODO: clean up path logic
+    let cur_dir = env::current_dir().expect("Can't access current working directory");
+    let clif_isle = cur_dir.join("../../../codegen/src").join("inst_specs.isle");
+    let prelude_isle = cur_dir.join("../../../codegen/src").join("prelude.isle");
+    let prelude_lower_isle = cur_dir
+        .join("../../../codegen/src")
+        .join("prelude_lower.isle");
+    let mut inputs = vec![
+        build_clif_lower_isle(),
+        prelude_isle,
+        prelude_lower_isle,
+        clif_isle,
+    ];
+    inputs.push(PathBuf::from(file));
+
+    let lexer = cranelift_isle::lexer::Lexer::from_files(&inputs).unwrap();
+    let defs = cranelift_isle::parser::parse(lexer).expect("should parse");
+    let (typeenv, termenv) = create_envs(&defs).unwrap();
+    let annotation_env = parse_annotations(&defs, &termenv, &typeenv);
+
+    let config = Config {
+        term: termname.clone(),
+        distinct_check: false,
+        custom_verification_condition: None,
+        custom_assumptions: None,
+        names: None,
+    };
+
+    // Get the types/widths for this particular term
+    let args = concrete.args.iter().map(|i| i.ty.clone()).collect();
+    let ret = concrete.output.ty;
+    let t = TermSignature {
+        args,
+        ret,
+        canonical_type: None,
+    };
+
+    let type_sols = type_rules_with_term_and_types(
+        &termenv,
+        &typeenv,
+        &annotation_env,
+        &config,
+        &t,
+        &Some(concrete.clone()),
+    );
+    let result = verify_rules_for_term(&termenv, &typeenv, &type_sols, t, &Some(concrete), &config);
+    assert_eq!(result, VerificationResult::Success);
+}
