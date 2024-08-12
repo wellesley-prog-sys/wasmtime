b11434ec9dff2a67239b068001d855e3f307d443
Michael McLoughlin <mcloughlin@cmu.edu>
2024-08-12 17:03:47 +0100

    direct replay smt to temp directory
    Original-commit: https://github.com/mmcloughlin/veriisle-wasmtime/commit/89363bfe95a35040e5bd522b3e7c54f2648d1fc5

--- cranelift/isle/veri/script/veri.sh
+++ cranelift/isle/veri/script/veri.sh
@@ -11,7 +11,7 @@ cargo run --bin veri -- \
     --codegen-crate-dir ../../codegen/ \
     --work-dir /tmp \
     --name "${arch}" \
-    --smt2-replay-path "output/${arch}.smt2" \
+    --smt2-replay-path "tmp/${arch}.smt2" \
     --timeout 10 \
     "$@" \
     | tee "output/${arch}.veri"
