351edf53944dcd0c853383cebc6cebc15c0a4299
Michael McLoughlin <mcloughlin@cmu.edu>
2024-08-25 19:26:24 -0400

    explorer: generate for x64 as well
    Original-commit: https://github.com/mmcloughlin/veriisle-wasmtime/commit/fcc3cb7364288e69d228bb100ab9f7bed9a6344a

--- cranelift/isle/veri/script/explorer.sh
+++ cranelift/isle/veri/script/explorer.sh
@@ -6,8 +6,11 @@ export RUST_LOG=info
 
 output_dir="tmp/explorer"
 rm -rf "${output_dir}"
-cargo run --bin explorer -- \
-    --codegen-crate-dir ../../codegen/ \
-    --work-dir /tmp \
-    --name aarch64 \
-    --output-dir "${output_dir}"
+
+for arch in aarch64 x64; do
+    cargo run --bin explorer -- \
+        --codegen-crate-dir ../../codegen/ \
+        --work-dir /tmp \
+        --name "${arch}" \
+        --output-dir "${output_dir}/${arch}"
+done
