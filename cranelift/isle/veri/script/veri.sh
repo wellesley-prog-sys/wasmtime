c506269e2dedf20183b52cb34986f473e6b11eaa
Michael McLoughlin <mcloughlin@cmu.edu>
2024-08-28 20:48:54 -0400

    veri script options
    Original-commit: https://github.com/mmcloughlin/veriisle-wasmtime/commit/6c45a144aa8c2dd5befd7f93aab05d2ee3fb1e29

--- cranelift/isle/veri/script/veri.sh
+++ cranelift/isle/veri/script/veri.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env bash
 
-set -exuo pipefail
+set -euo pipefail
 
-# Clean.
-rm -f output/*.{veri,smt2}
+# Options
+function usage() {
+    echo "Usage: ${0} [-h] [-a <arch>] [-t <tmp_dir>] [-o <output_dir>]"
+    exit 2
+}
 
-# Verify some rules.
 arch="aarch64"
+tmp_dir="${TMPDIR}"
+output_dir="output"
+while getopts "a:t:o:h" opt; do
+    case "${opt}" in
+        a) arch="${OPTARG}" ;;
+        t) tmp_dir="${OPTARG}" ;;
+        o) output_dir="${OPTARG}" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Setup output.
+mkdir -p "${output_dir}"
+
+# Run.
 cargo run --bin veri -- \
     --codegen-crate-dir ../../codegen/ \
-    --work-dir /tmp \
+    --work-dir "${tmp_dir}" \
     --name "${arch}" \
-    --smt2-replay-path "tmp/${arch}.smt2" \
+    --smt2-replay-path /dev/null \
     --timeout 10 \
     "$@" \
-    | tee "output/${arch}.veri"
+    | tee "${output_dir}/${arch}.veri"
