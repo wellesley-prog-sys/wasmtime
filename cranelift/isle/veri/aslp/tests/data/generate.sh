c8de6a9cd83f194367e8393088635af3d7e733c4
Michael McLoughlin <mcloughlin@cmu.edu>
2024-09-24 14:00:14 -0400

    aslp: generate load instruction test data (#75)
    Generate `aslp` test cases for load instructions.
    
    Updates #35

--- cranelift/isle/veri/aslp/tests/data/generate.sh
+++ cranelift/isle/veri/aslp/tests/data/generate.sh
@@ -34,6 +34,7 @@ function testcase() {
 }
 
 # Testcases.
+# Arithmetic.
 testcase    "add"           "add x4, x5, x6"
 testcase    "add32"         "add w4, w5, w6"
 testcase    "add_imm"       "add x7, x8, #291"
@@ -43,10 +44,36 @@ testcase    "addc"          "adc x4, x5, x6"
 testcase    "adds"          "adds x1, x2, x3"
 testcase    "subs"          "subs x10, x11, x12, lsl 23"
 testcase    "subsp"         "sub sp, sp, #32"
+
+# 8-bit loads.
+testcase    "ldrb"                      "ldrb w1, [x2]"
+testcase    "ldrb_reg_reg"              "ldrb w1, [x2, x3]"
+testcase    "ldrb_reg_scale"            "ldrb w1, [x2, x3, lsl #0]"
+testcase    "ldrb_reg_scale_ext_uxtw"   "ldrb w1, [x2, w3, uxtw #0]"
+testcase    "ldrb_reg_scale_ext_sxtw"   "ldrb w1, [x2, w3, sxtw #0]"
+testcase    "ldrb_reg_scale_ext_sxtx"   "ldrb w1, [x2, x3, sxtx #0]"
+
+# 16-bit loads.
+testcase    "ldrh"                      "ldrh w1, [x2]"
+testcase    "ldrh_reg_reg"              "ldrh w1, [x2, x3]"
+testcase    "ldrh_reg_scale"            "ldrh w1, [x2, x3, lsl #0]"
+testcase    "ldrh_reg_scale1"           "ldrh w1, [x2, x3, lsl #1]"
+testcase    "ldrh_reg_scale_ext_uxtw"   "ldrh w1, [x2, w3, uxtw #0]"
+testcase    "ldrh_reg_scale_ext_sxtw"   "ldrh w1, [x2, w3, sxtw #0]"
+testcase    "ldrh_reg_scale_ext_sxtx"   "ldrh w1, [x2, x3, sxtx #0]"
+
+# 32-bit loads.
+testcase    "ldrsw_reg_reg"              "ldrsw x1, [x2, x3]"
+testcase    "ldrsw_reg_scale"            "ldrsw x1, [x2, x3, lsl #0]"
+testcase    "ldrsw_reg_scale2"           "ldrsw x1, [x2, x3, lsl #2]"
+testcase    "ldrsw_reg_scale_ext_uxtw"   "ldrsw x1, [x2, w3, uxtw #0]"
+testcase    "ldrsw_reg_scale_ext_sxtw"   "ldrsw x1, [x2, w3, sxtw #0]"
+testcase    "ldrsw_reg_scale_ext_sxtx"   "ldrsw x1, [x2, x3, sxtx #0]"
+
+# Misc.
 testcase    "csel"          "csel x10, x12, x14, hs"
 testcase    "ccmp"          "ccmp x22, x1, 13, eq"
 testcase    "clz"           "clz x15, x3"
-testcase    "uload8"        "ldrb w1, [x2]"
 testcase    "ldp"           "ldp x1, x2, [x3], #128"
 testcase    "stp"           "stp x1, x2, [x3], #128"
 testcase    "ucvtf"         "ucvtf d0, w2"
