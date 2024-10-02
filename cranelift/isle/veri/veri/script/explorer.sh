949c8daa5257ef7742bae6f3b40359d437e2f686
Michael McLoughlin <mcloughlin@cmu.edu>
2024-10-02 19:39:35 -0400

    explorer: output path options (#105)
    Configurable output options for the explorer scripts.

--- cranelift/isle/veri/veri/script/explorer.sh
+++ cranelift/isle/veri/veri/script/explorer.sh
@@ -4,13 +4,33 @@ set -exuo pipefail
 
 export RUST_LOG=info
 
-output_dir="tmp/explorer"
-rm -rf "${output_dir}"
+# Options
+function usage() {
+    echo "Usage: ${0} [-h] [-o <output_dir>]"
+    exit 2
+}
 
+output_dir="${ISLE_EXPLORER_OUTPUT_DIR}"
+while getopts "o:h" opt; do
+    case "${opt}" in
+        o) output_dir="${OPTARG}" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+if [[ ! -d "${output_dir}" ]]; then
+    echo "output directory does not exist"
+    exit 1
+fi
+
+# Generate explorer.
 for arch in aarch64 x64; do
+    arch_dir="${output_dir:?}/${arch:?}"
+    rm -rf "${arch_dir}"
     cargo run --bin explorer -- \
         --codegen-crate-dir ../../../codegen/ \
         --work-dir /tmp \
         --name "${arch}" \
-        --output-dir "${output_dir}/${arch}"
+        --output-dir "${arch_dir}"
 done
