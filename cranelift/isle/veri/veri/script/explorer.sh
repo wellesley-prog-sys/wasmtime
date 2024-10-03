wellesley-prog-sys/wasmtime

commit 949c8daa5257ef7742bae6f3b40359d437e2f686

explorer: output path options (#105)
Configurable output options for the explorer scripts.

cranelift/isle/veri/veri/script/explorer.sh
@@ -4,13 +4,33 @@ set -exuo pipefail
 
 export RUST_LOG=info
 
-output_dir="tmp/explorer"
-rm -rf "${output_dir}"
+# Options
+function usage() {
+    echo "Usage: ${0} [-h] [-o <output_dir>]"
+    exit 2
+}
 
+output_dir="${ISLE_EXPLORER_OUTPUT_DIR}"
+while getopts "o:h" opt; do
+    case "${opt}" in
+        o) output_dir="${OPTARG}" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+if [[ ! -d "${output_dir}" ]]; then
+    echo "output directory does not exist"
+    exit 1
+fi
+
+# Generate explorer.
 for arch in aarch64 x64; do
+    arch_dir="${output_dir:?}/${arch:?}"
+    rm -rf "${arch_dir}"
     cargo run --bin explorer -- \
         --codegen-crate-dir ../../../codegen/ \
         --work-dir /tmp \
         --name "${arch}" \
-        --output-dir "${output_dir}/${arch}"
+        --output-dir "${arch_dir}"
 done

cranelift/isle/veri/veri/script/serve.sh
@@ -2,9 +2,29 @@
 
 set -exuo pipefail
 
-output_dir="tmp/explorer"
-port="5050"
+# Options
+function usage() {
+    echo "Usage: ${0} [-h] [-o <output_dir>] [-p <port>]"
+    exit 2
+}
 
+output_dir="${ISLE_EXPLORER_OUTPUT_DIR}"
+port="5050"
+while getopts "o:p:h" opt; do
+    case "${opt}" in
+        o) output_dir="${OPTARG}" ;;
+        p) port="${OPTARG}" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+if [[ ! -d "${output_dir}" ]]; then
+    echo "output directory does not exist"
+    exit 1
+fi
+
+# Serve.
 miniserve \
     --port "${port}" \
     --index index.html \

commit 37d259728f748ea2b405c7e6d417d9d4bb9b84cf

ci: fix explorer deploy (#106)
Fixes the explorer deploy broken by #105.

.github/workflows/explorer.yml
@@ -5,6 +5,12 @@ on:
     branches:
       - veriisle
   workflow_dispatch:
+  pull_request:
+    branches:
+      - veriisle
+    paths:
+      - '.github/workflows/explorer.yml'
+      - 'cranelift/isle/veri/veri/script/explorer.sh'
 
 # Sets permissions to allow deployment to Github Pages.
 permissions:
@@ -28,13 +34,16 @@ jobs:
         run: sudo apt-get install -y graphviz
       - name: Build Explorer
         working-directory: cranelift/isle/veri/veri
-        run: ./script/explorer.sh
+        run: ./script/explorer.sh -o "${{ runner.temp }}/explorer"
         env:
           ISLE_EXPLORER_GRAPHS: 'true'
       - name: Upload artifact
+        if: github.event_name != 'pull_request'
+        id: upload
         uses: actions/upload-pages-artifact@v2
         with:
-          path: cranelift/isle/veri/veri/tmp/explorer
+          path: ${{ runner.temp }}/explorer
       - name: Deploy to GitHub Pages
+        if: steps.upload.outcome == 'success'
         id: deployment
         uses: actions/deploy-pages@v2

cranelift/isle/veri/veri/script/explorer.sh
@@ -10,7 +10,7 @@ function usage() {
     exit 2
 }
 
-output_dir="${ISLE_EXPLORER_OUTPUT_DIR}"
+output_dir="${ISLE_EXPLORER_OUTPUT_DIR:-}"
 while getopts "o:h" opt; do
     case "${opt}" in
         o) output_dir="${OPTARG}" ;;
@@ -19,10 +19,12 @@ while getopts "o:h" opt; do
     esac
 done
 
-if [[ ! -d "${output_dir}" ]]; then
-    echo "output directory does not exist"
-    exit 1
+# Setup output.
+if [[ -z "${output_dir}" ]]; then
+    echo "output directory not set"
+    exit 2
 fi
+mkdir -p "${output_dir}"
 
 # Generate explorer.
 for arch in aarch64 x64; do

cranelift/isle/veri/veri/script/serve.sh
@@ -8,7 +8,7 @@ function usage() {
     exit 2
 }
 
-output_dir="${ISLE_EXPLORER_OUTPUT_DIR}"
+output_dir="${ISLE_EXPLORER_OUTPUT_DIR:-}"
 port="5050"
 while getopts "o:p:h" opt; do
     case "${opt}" in