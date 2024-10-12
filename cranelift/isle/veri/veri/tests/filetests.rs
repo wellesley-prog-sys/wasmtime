b8d1646d735879b190f6017b4bc3dc8d811592a8
Michael McLoughlin <mcloughlin@cmu.edu>
2024-10-12 19:59:52 -0400

    tests: use temporary log directory (#147)
    Use temporary log directory in filetests.
    
    Prevents the tests from conflicting with each other.

--- cranelift/isle/veri/veri/tests/filetests.rs
+++ cranelift/isle/veri/veri/tests/filetests.rs
@@ -2,11 +2,14 @@ use std::path::PathBuf;
 
 use cranelift_isle_veri::runner::Runner;
 use cranelift_isle_veri_test_macros::file_tests;
+use tempfile::tempdir;
 
 #[file_tests(path = "filetests/pass", ext = "isle")]
 fn pass(test_file: &str) {
     let inputs = vec![PathBuf::from(test_file)];
     let mut runner = Runner::from_files(&inputs).expect("should be able to create runner");
+    let temp_dir = tempdir().expect("should be able to create temporary log directory");
+    runner.set_log_dir(temp_dir.path().join("log"));
     runner.include_first_rule_named();
     runner.set_root_term("test");
     runner.run().expect("verification should pass");
@@ -16,6 +19,8 @@ fn pass(test_file: &str) {
 fn broken(test_file: &str) {
     let inputs = vec![PathBuf::from(test_file)];
     let mut runner = Runner::from_files(&inputs).expect("should be able to create runner");
+    let temp_dir = tempdir().expect("should be able to create temporary log directory");
+    runner.set_log_dir(temp_dir.path().join("log"));
     runner.include_first_rule_named();
     runner.set_root_term("test");
     runner.run().expect_err("verification should fail");
