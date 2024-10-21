7c57a36814b23b35b71b1b9f958052e4a54be61e
Jaya Chintam <jc103@wellesley.edu>
2024-10-21 17:50:57 +0000

    Added helper for zero-extend

--- cranelift/isle/veri/veri/src/encoded/popcnt.rs
+++ cranelift/isle/veri/veri/src/encoded/popcnt.rs
@@ -6,9 +6,23 @@ fn declare(smt: &mut Context, name: String, val: SExpr) -> SExpr {
     smt.atom(name)
 }
 
+fn zero_extend(smt: &mut Context, padding: usize, value: SExpr) -> SExpr {
+    if padding == 0 {
+        return value;
+    }
+    smt.list(vec![
+        smt.list(vec![
+            smt.atoms().und,
+            smt.atom("zero_extend"),
+            smt.numeral(padding),
+        ]),
+        value,
+    ])
+}
+
 pub fn popcnt(smt: &mut Context, ty: usize, x: SExpr, id: u32) -> SExpr {
     let mut bits: Vec<_> = (0..ty)
-        .map(|i| smt.zero_extend(7, smt.extract(i as i32, i as i32, x)))
+        .map(|i| zero_extend(smt, 7, smt.extract(i as i32, i as i32, x)))
         .collect();
     let initial = bits.pop().unwrap();
     let r = bits.iter().fold(initial, |a, b| smt.bvadd(a, *b));
